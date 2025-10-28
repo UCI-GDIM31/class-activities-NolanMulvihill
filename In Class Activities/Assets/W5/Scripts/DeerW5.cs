dca6acc5f79c9f8d561a0a22e59fa973838be9d5
NolanMulvihill <nolanmulvihill15@gmail.com>
2025-10-27 18:46:30 -0700

    random extera files from pull

--- In Class Activities/Assets/W5/Scripts/DeerW5.cs
+++ In Class Activities/Assets/W5/Scripts/DeerW5.cs
@@ -1,6 +1,18 @@
 using UnityEngine;
 using UnityEngine.AI;
 
+public class DeerW5 : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private NavMeshAgent deer;
+
+    private void Start(){
+        deer = gameObject.GetComponent<NavMeshAgent>();
+        deer.SetDestination(target.position);
+
+    }
+}
+
 // Write your DeerW5 class in here :)
 // Hint: if you don't remember what a class is supposed to look like,
 //      maybe check out CatW5...
